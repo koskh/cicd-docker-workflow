19559bb6d4801e5eb3f4fbc35449876cab3c6102
K. Khabalkin <koskh@yandex.ru>
2025-08-26 21:47:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello from workflow for ICH. ########\n');
+  res.end('@@@@ Hello from workflow for ICH. @@@@\n');
 });
 
 server.listen(port, () => {
